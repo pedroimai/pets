08db006e6a1d2217e94a30e11b898da7bb7c4bcc
pedro.gomes <pedro.gomes@luizalabs.com>
2020-09-20 15:05:18 -0300

    config hilt viewmodel injection

--- app/src/main/java/com/pedroimai/pets/listing/ListingFragment.kt
+++ app/src/main/java/com/pedroimai/pets/listing/ListingFragment.kt
@@ -1,32 +1,13 @@
 package com.pedroimai.pets.listing
 
-import androidx.lifecycle.ViewModelProviders
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.pedroimai.pets.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ListingFragment : Fragment() {
+class ListingFragment : Fragment(R.layout.fragment_listing) {
 
-    companion object {
-        fun newInstance() = ListingFragment()
-    }
-
-    private lateinit var viewModel: ListingViewModel
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_listing, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(ListingViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+    private val viewModel: ListingViewModel by viewModels()
 
 }
