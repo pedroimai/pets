30c64420ffaeb17d1b23aa235eef1197aed9dc58
pedro.gomes <pedro.gomes@luizalabs.com>
2020-09-20 13:27:34 -0300

    add navigation component

--- app/src/main/java/com/pedroimai/pets/MainActivity.kt
+++ app/src/main/java/com/pedroimai/pets/MainActivity.kt
@@ -1,20 +1,7 @@
 package com.pedroimai.pets
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.pedroimai.pets.listing.ListingFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, ListingFragment.newInstance())
-                    .commitNow()
-        }
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.main_activity)
