364e61580dbe3b6f4fa6338d0b4fe830814ab900
pedro.gomes <pedro.gomes@luizalabs.com>
2020-09-21 10:01:58 -0300

    use listing viewmodel in Listing Fragment

--- app/src/main/java/com/pedroimai/pets/listing/ListingFragment.kt
+++ app/src/main/java/com/pedroimai/pets/listing/ListingFragment.kt
@@ -1,13 +1,32 @@
 package com.pedroimai.pets.listing
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.pedroimai.pets.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ListingFragment : Fragment(R.layout.fragment_listing) {
 
     private val viewModel: ListingViewModel by viewModels()
+    private val uiScope = CoroutineScope(Dispatchers.Main)
+
+    @ExperimentalCoroutinesApi
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        uiScope.launch {
+            viewModel.getBreeds().observe(viewLifecycleOwner, { breeds ->
+                println("getBreeds: $breeds")
+
+            })
+        }
+    }
 
 }
