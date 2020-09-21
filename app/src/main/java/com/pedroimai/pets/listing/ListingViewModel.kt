2101e6dc4446197902470638a2cca31fa98ecc2d
pedro.gomes <pedro.gomes@luizalabs.com>
2020-09-21 10:01:25 -0300

    use dogservice in listing breeds viewmodel

--- app/src/main/java/com/pedroimai/pets/listing/ListingViewModel.kt
+++ app/src/main/java/com/pedroimai/pets/listing/ListingViewModel.kt
@@ -1,8 +1,15 @@
 package com.pedroimai.pets.listing
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.pedroimai.pets.networking.Breed
+import com.pedroimai.pets.networking.DogService
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
-class ListingViewModel @ViewModelInject constructor() : ViewModel() {
-    // TODO: Implement the ViewModel
+class ListingViewModel @ViewModelInject constructor(private val api: DogService) : ViewModel() {
+    @ExperimentalCoroutinesApi
+    suspend fun getBreeds(): LiveData<List<Breed>> =
+        api.getBreeds().asLiveData()
 }
